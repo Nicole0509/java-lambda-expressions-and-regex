2e5194190f42666537d6cca77f0b457c77dd4a20
Nicole0509 <niashavril0509@gmail.com>
2025-09-25 21:13:23 +0200

    Succesffully used my first lambda function

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Revising Lambda expressions and RegEx!");
+
+        FunctionalInterfaceExample funExample = () -> {
+            System.out.println("This is my first time using Lambda functions in Java:)");
+        };
+
+        funExample.doSomething();
      }
 }
